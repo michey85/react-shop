99f994cff15538a16065b5ac699bc4b01eb14ec4
Mikhail Nepomnyashchiy <michey85@gmail.com>
2020-12-21 14:50:46 +0300

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,12 @@ function Footer() {
             <div className='footer-copyright'>
                 <div className='container'>
                     © {new Date().getFullYear()} Copyright Text
-                    <a className='grey-text text-lighten-4 right' href='#!'>
+                    <a
+                        className='grey-text text-lighten-4 right'
+                        href='https://github.com/michey85/react-shop'
+                        rel='noreferrer'
+                        target='_blank'
+                    >
                         Repo
                     </a>
                 </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ function Header() {
     return (
         <nav className='green darken-1'>
             <div className='nav-wrapper'>
-                <a href='#!' className='brand-logo'>
+                <a href='/' className='brand-logo'>
                     React Shop
                 </a>
                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
                     <li>
-                        <a href='!#'>Repo</a>
+                        <a
+                            href='https://github.com/michey85/react-shop'
+                            target='_blank'
+                            rel='noreferrer'
+                        >
+                            Repo
+                        </a>
                     </li>
                 </ul>
             </div>
