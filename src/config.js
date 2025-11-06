42a05ed7699fcb5da26b06ad24679b64bff57198
Mikhail Nepomniashchii <michey85@gmail.com>
2025-11-06 17:12:37 +0100

    update react version

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const API_URL = 'https://fortniteapi.io/v1/shop?lang=ru';
+const API_URL = 'https://fortniteapi.io/v2/shop?lang=ru';
 
 export { API_KEY, API_URL };
 
