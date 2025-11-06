75c08b7afeff67a1b9f8a2e65c421e474655b4f1
Mikhail Nepomniashchii <michey85@gmail.com>
2025-11-06 17:03:33 +0100

    rsbuild migration

---
+++ rsbuild.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, loadEnv } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
+const { publicVars, rawPublicVars } = loadEnv({ prefixes: ['REACT_APP_'] });
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  html: {
+    template: './public/index.html',
+  },
+  output: {
+    distPath: {
+      root: 'build',
+    },
+  },
+  source: {
+    define: {
+      ...publicVars,
+      'process.env': JSON.stringify(rawPublicVars),
+    },
+  },
+});
