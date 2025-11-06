9e5cdc74afa482a7fa9d9d57094a5e29c61cfb4e
Mikhail Nepomniashchii <michey85@gmail.com>
2025-11-06 17:47:28 +0100

    rsbuild dist folder setup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React Shop Portfolio Project
 
 [Demo](https://michey85.github.io/react-shop).
+
+В качестве API используется fortniteapi.io shop v2.
--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   output: {
     polyfill: 'usage',
     distPath: {
-      root: 'build',
+      root: 'dist',
     },
   },
   source: {
